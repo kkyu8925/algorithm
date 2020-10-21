eb2ac19adde8f1bfdcb60f74a6580b190eba35f7
gusrb8925 <gusrb8925@naver.com>
2020-10-21 10:38:19 +0900

    프로그래머스 자바 lv1 평균 구하기

---
+++ Java/src/lv1/AVG.java
@@ -0,0 +1,22 @@
+package lv1;
+
+public class AVG {
+	public static double solution(int[] arr) {
+		double answer = 0;
+		double sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		answer = sum / arr.length;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4 };
+		int[] ar = { 5, 5 };
+		System.out.println(solution(arr));
+		System.out.println(solution(ar));
+
+	}
+
+}
