0b09347935ea54d1516e321fb9ec58a1730ba38a
gusrb8925 <gusrb8925@naver.com>
2020-10-23 15:36:45 +0900

    프로그래머스 자바 lv1 서울에서 김서방 찾기

---
+++ Java/src/lv1/FindKim.java
@@ -0,0 +1,22 @@
+package lv1;
+
+public class FindKim {
+	public static String solution(String[] s) {
+		String answer = "";
+		String k = "Kim";
+		int i = 0;
+		for (i = 0; i < s.length; i++) {
+			if (s[i].equals(k)) {
+				break;
+			}
+		}
+		answer = "김서방은 " + i + "에 있다";
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String[] s = { "jane", "Kim" };
+		System.out.println(solution(s));
+	}
+
+}
