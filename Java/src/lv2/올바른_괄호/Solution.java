e3dcf0785bf41554dfe750f372ff7e8a3654b338
kkyu <gusrb8925@naver.com>
2021-04-02 20:08:29 +0900

    21-04-02

---
+++ Java/src/lv2/올바른_괄호/Solution.java
@@ -0,0 +1,45 @@
+package lv2.올바른_괄호;
+
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (stack.empty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+
+    boolean solution2(String s) {
+        boolean answer = false;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            }
+            if (s.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                break;
+            }
+        }
+        if (count == 0) {
+            answer = true;
+        }
+
+        return answer;
+    }
+
+}
---
+++ Java/src/lv2/피보나치수/Solution.java
@@ -0,0 +1,15 @@
+package lv2.피보나치수;
+
+class Solution {
+    public int solution(int n) {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n; i++) {
+            int tmp = (a + b) % 1234567;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+}
