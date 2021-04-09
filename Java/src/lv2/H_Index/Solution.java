205272d8e45d42dce0dad30ffd8a29a24ae0fa25
kkyu <gusrb8925@naver.com>
2021-04-09 20:40:09 +0900

    21-04-09

---
+++ Java/src/lv2/H_Index/Solution.java
@@ -0,0 +1,23 @@
+package lv2.H_Index;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        for (int i = 1; i <= citations.length; i++) {
+            int cnt = 0;
+
+            for (int citation : citations) {
+                if (i <= citation) {
+                    cnt++;
+                }
+            }
+
+            if (cnt >= i) {
+                answer = i;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ Java/src/lv2/H_Index/Solution2.java
@@ -0,0 +1,16 @@
+package lv2.H_Index;
+
+import java.util.Arrays;
+
+class Solution2 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int tmp = Math.min(citations[i], citations.length - i);
+            answer = Math.max(answer, tmp);
+        }
+        return answer;
+    }
+}
