41f8c5475bbd4e6b18ebfe0c2be68f811db1778c
gusrb8925 <gusrb8925@naver.com>
2021-07-06 01:15:41 +0900

    21-06-18 two pointer & sliding window

---
+++ Java/src/inflearn/section03/ex04_연속_부분수열/Main.java
@@ -0,0 +1,37 @@
+package inflearn.section03.ex04_연속_부분수열;
+
+import java.util.Scanner;
+
+public class Main {
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int sum = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) {
+                answer++;
+            }
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
+}
