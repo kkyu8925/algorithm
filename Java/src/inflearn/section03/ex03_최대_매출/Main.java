3f20155f8eb9c6968517478cb67a16ea4c9cf243
gusrb8925 <gusrb8925@naver.com>
2021-07-06 01:14:55 +0900

    21-06-18 sliding window

---
+++ Java/src/inflearn/section03/ex03_최대_매출/Main.java
@@ -0,0 +1,37 @@
+package inflearn.section03.ex03_최대_매출;
+
+import java.util.Scanner;
+
+public class Main {
+    private int solution(int n, int k, int[] arr) {
+        int answer;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        answer = sum;
+
+        for (int i = k; i < n; i++) {
+            sum += arr[i];
+            sum -= arr[i - k];
+
+            if (answer < sum) {
+                answer = sum;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
