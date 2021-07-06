3f4e3fe8e71886b8dfb3f39cb7c3e481f17ed5e2
gusrb8925 <gusrb8925@naver.com>
2021-07-07 02:26:23 +0900

    21-06-27 recursive & memoization

---
+++ Java/src/inflearn/section07/ex04_피보나치_재귀/Main.java
@@ -0,0 +1,40 @@
+package inflearn.section07.ex04_피보나치_재귀;
+
+public class Main {
+    static int[] fi;
+
+    private int memoization(int n) {
+        if (fi[n] > 0) {
+            return fi[n];
+        }
+
+        if (n < 2) {
+            return fi[n] = 1;
+        } else {
+            return fi[n] = memoization(n - 1) + memoization(n - 2);
+        }
+    }
+
+    private int DFS(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return DFS(n - 1) + DFS(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        int n = 10;
+//        for (int i = 1; i <= n; i++) {
+//            System.out.println(T.DFS(i));
+//        }
+
+        // memoization
+        fi = new int[n + 1];
+        T.memoization(n);
+        for (int i = 0; i < n; i++) {
+            System.out.println(fi[i]);
+        }
+    }
+}
