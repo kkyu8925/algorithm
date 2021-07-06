6303a41dcec8c1ba9e508340baf32b8363befa5c
gusrb8925 <gusrb8925@naver.com>
2021-07-07 02:20:49 +0900

    21-06-24 bubble sort

---
+++ Java/src/inflearn/section06/ex02_버블정렬/Main.java
@@ -0,0 +1,31 @@
+package inflearn.section06.ex02_버블정렬;
+
+import java.util.Scanner;
+
+public class Main {
+    private int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
