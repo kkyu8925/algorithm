18db03c8495425e2f63243ede280b65a785b1055
gusrb8925 <gusrb8925@naver.com>
2021-07-07 02:21:35 +0900

    21-06-25 Least Recently Used

---
+++ Java/src/inflearn/section06/ex04_LRU/Main.java
@@ -0,0 +1,44 @@
+package inflearn.section06.ex04_LRU;
+
+import java.util.Scanner;
+
+public class Main {
+    private int[] solution(int size, int n, int[] arr) {
+        int[] cache = new int[size];
+        for (int num : arr) {
+            int idx = -1;
+            for (int i = 0; i < size; i++) {
+                if (cache[i] == num) {
+                    idx = i;
+                }
+            }
+
+            if (idx == -1) {
+                for (int i = size - 1; i > 0; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            } else {
+                for (int i = idx; i > 0; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            }
+
+            cache[0] = num;
+        }
+        return cache;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : T.solution(size, n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
