6e64aedb0b212f147230fad92a1e8702c692682c
gusrb8925 <gusrb8925@naver.com>
2021-08-24 16:45:53 +0900

    21-08-24 recursive

--- Java/src/inflearn/section07/ex01_재귀함수/Main.java
+++ Java/src/inflearn/section07/ex01_재귀함수/Main.java
@@ -2,12 +2,13 @@ package inflearn.section07.ex01_재귀함수;
 
 public class Main {
     private void DFS(int i) {
-        if (i == 0) {
-            return;
-        } else {
-            DFS(i - 1);
-            System.out.print(i + " ");
+        if (i == 0) return;
+        else {
+            // 출력 - 1 2 3 4 5 6 7 8 9 10
+//            DFS(i - 1);
+//            System.out.print(i + " ");
 
+            // 출력 - 10 9 8 7 6 5 4 3 2 1
 //            System.out.print(i + " ");
 //            DFS(i - 1);
         }
