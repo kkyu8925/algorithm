691e941380257fe42b2d5847af771ef725c3badc
gusrb8925 <gusrb8925@naver.com>
2021-08-24 20:08:56 +0900

    21-08-24 BFS(Breadth-First Search) 레벨 탐색 순회 - 최단거리

--- Java/src/inflearn/section07/ex08_송아지_찾기/Main.java
+++ Java/src/inflearn/section07/ex08_송아지_찾기/Main.java
@@ -2,42 +2,46 @@ package inflearn.section07.ex08_송아지_찾기;
 
 import java.util.*;
 
+/**
+ * BFS = 최단거리
+ */
 public class Main {
-    int[] dis = {1, -1, 5};
-    Set<Integer> set = new HashSet<>(); // 중복 체크
+    int[] disArr = {1, -1, 5};
+    Set<Integer> check = new HashSet<>(); // 중복 체크
     Queue<Integer> que = new LinkedList<>();
 
-    private int BFS(int s, int e) {
-        set.add(s);
-        que.offer(s);
+    private int BFS(int start, int arrival) {
+        check.add(start);
+        que.offer(start);
         int L = 0;
         while (!que.isEmpty()) {
             int len = que.size();
             for (int i = 0; i < len; i++) {
                 int current = que.poll();
-                for (int x : dis) {
-                    int next = current + x;
 
-                    if (next == e) {
-                        return L + 1;
-                    }
+                if (current == arrival) {
+                    return L;
+                }
 
-                    if (!set.contains(next)) {
-                        set.add(next);
+                for (int dis : disArr) {
+                    int next = current + dis;
+
+                    if (!check.contains(next) && next > 0) {
+                        check.add(next);
                         que.offer(next);
                     }
                 }
             }
             L++;
         }
-        return 0;
+        return -1;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();
-        int e = sc.nextInt();
-        System.out.println(T.BFS(s, e));
+        int start = sc.nextInt();
+        int arrival = sc.nextInt();
+        System.out.println(T.BFS(start, arrival));
     }
 }
