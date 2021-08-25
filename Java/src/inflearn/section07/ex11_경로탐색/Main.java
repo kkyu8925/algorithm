1fb03e8a602cea024c0699e94f636f094278a32d
gusrb8925 <gusrb8925@naver.com>
2021-08-25 20:01:43 +0900

    21-08-25 그래프(Graph) - DFS(Depth-First Search)

--- Java/src/inflearn/section07/ex11_경로탐색/Main.java
+++ Java/src/inflearn/section07/ex11_경로탐색/Main.java
@@ -3,16 +3,17 @@ package inflearn.section07.ex11_경로탐색;
 import java.util.Scanner;
 
 public class Main {
-    static int n, m, answer = 0;
+    // N-Node 개수, E-Edge 개수
+    static int N, E, answer = 0;
     static int[][] graph;
     static int[] check;
 
-    public void DFS(int v) {
-        if (v == n) {
+    public void DFS(int Node) {
+        if (Node == N) {
             answer++;
         } else {
-            for (int i = 1; i <= n; i++) {
-                if (graph[v][i] == 1 && check[i] == 0) {
+            for (int i = 1; i <= N; i++) {
+                if (graph[Node][i] == 1 && check[i] == 0) {
                     check[i] = 1;
                     DFS(i);
                     check[i] = 0;
@@ -24,11 +25,11 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        graph = new int[n + 1][m + 1];
-        check = new int[n + 1];
-        for (int i = 0; i < m; i++) {
+        N = sc.nextInt();
+        E = sc.nextInt();
+        graph = new int[N + 1][E + 1];
+        check = new int[N + 1];
+        for (int i = 0; i < E; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             graph[a][b] = 1;
