e2f39bda62f0d93f6a4a0971d6065d2bc44dad13
gusrb8925 <gusrb8925@naver.com>
2021-08-28 16:51:32 +0900

    21-08-28 BFS(Breadth-First Search) 활용

--- Java/src/inflearn/section08/ex12_토마토/Main.java
+++ Java/src/inflearn/section08/ex12_토마토/Main.java
@@ -14,22 +14,22 @@ class Point {
 }
 
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] board, dis;
-    static int n, m;
+    static int[] DX = {-1, 0, 1, 0};
+    static int[] DY = {0, 1, 0, -1};
+    static int[][] BOARD, DIS;
+    static int N, M; // 행,렬
     static Queue<Point> que = new LinkedList<>();
 
-    private void BFS() {
+    private void bfs() {
         while (!que.isEmpty()) {
             Point now = que.poll();
             for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && board[nextX][nextY] == 0) {
-                    board[nextX][nextY] = 1;
+                int nextX = now.x + DX[i];
+                int nextY = now.y + DY[i];
+                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && BOARD[nextX][nextY] == 0) {
+                    BOARD[nextX][nextY] = 1;
                     que.offer(new Point(nextX, nextY));
-                    dis[nextX][nextY] = dis[now.x][now.y] + 1;
+                    DIS[nextX][nextY] = DIS[now.x][now.y] + 1;
                 }
             }
         }
@@ -38,36 +38,42 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
-        board = new int[n][m];
-        dis = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = sc.nextInt();
-                if (board[i][j] == 1) {
+        M = sc.nextInt();
+        N = sc.nextInt();
+        BOARD = new int[N][M];
+        DIS = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                BOARD[i][j] = sc.nextInt();
+                if (BOARD[i][j] == 1) {
                     que.offer(new Point(i, j));
                 }
             }
         }
-        T.BFS();
+
+        T.bfs();
+
         boolean flag = true;
         int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0) {
-                    flag = false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (BOARD[i][j] == 0) {
+                    flag = false; // 익지 않은 토마토 존재함(-1으로 막혀있을 때)
+                    break;
                 }
             }
         }
+
         if (flag) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    answer = Math.max(answer, dis[i][j]);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    answer = Math.max(answer, DIS[i][j]);
                 }
             }
             System.out.println(answer);
         } else {
+            // 익지 않은 토마토가 존재할 때
             System.out.println(-1);
         }
     }
