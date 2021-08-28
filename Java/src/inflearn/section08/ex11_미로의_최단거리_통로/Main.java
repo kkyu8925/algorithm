122f4e7c7418a893f50c5273eee5e8fc532da470
gusrb8925 <gusrb8925@naver.com>
2021-08-28 16:40:08 +0900

    21-08-28 BFS(Breadth-First Search) 활용

--- Java/src/inflearn/section08/ex11_미로의_최단거리_통로/Main.java
+++ Java/src/inflearn/section08/ex11_미로의_최단거리_통로/Main.java
@@ -14,23 +14,23 @@ class Point {
 }
 
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] board, dis;
+    static int[] DX = {-1, 0, 1, 0};
+    static int[] DY = {0, 1, 0, -1};
+    static int[][] BOARD, DIS;
 
-    public void BFS(int x, int y) {
+    public void bfs(int x, int y) {
         Queue<Point> que = new LinkedList<>();
         que.offer(new Point(x, y));
-        board[x][y] = 1;
+        BOARD[x][y] = 1;
         while (!que.isEmpty()) {
             Point now = que.poll();
             for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
-                if (nextX >= 1 && nextX < 8 && nextY >= 1 && nextY < 8 && board[nextX][nextY] == 0) {
-                    board[nextX][nextY] = 1;
+                int nextX = now.x + DX[i];
+                int nextY = now.y + DY[i];
+                if (nextX >= 1 && nextX < 8 && nextY >= 1 && nextY < 8 && BOARD[nextX][nextY] == 0) {
+                    BOARD[nextX][nextY] = 1;
                     que.offer(new Point(nextX, nextY));
-                    dis[nextX][nextY] = dis[now.x][now.y] + 1;
+                    DIS[nextX][nextY] = DIS[now.x][now.y] + 1;
                 }
             }
         }
@@ -39,18 +39,19 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        board = new int[8][8];
-        dis = new int[8][8];
+        BOARD = new int[8][8];
+        DIS = new int[8][8];
         for (int i = 1; i < 8; i++) {
             for (int j = 1; j < 8; j++) {
-                board[i][j] = sc.nextInt();
+                BOARD[i][j] = sc.nextInt();
             }
         }
-        T.BFS(1, 1);
-        if (dis[7][7] == 0) {
+
+        T.bfs(1, 1);
+        if (DIS[7][7] == 0) {
             System.out.println(-1);
         } else {
-            System.out.println(dis[7][7]);
+            System.out.println(DIS[7][7]);
         }
     }
 }
