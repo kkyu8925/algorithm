f299165f3e49480db6969ad3037aef1201326423
gusrb8925 <gusrb8925@naver.com>
2021-08-28 16:33:05 +0900

    21-08-28 DFS(Depth-First Search) 활용

--- Java/src/inflearn/section08/ex09_조합_구하기/Main.java
+++ Java/src/inflearn/section08/ex09_조합_구하기/Main.java
@@ -3,19 +3,19 @@ package inflearn.section08.ex09_조합_구하기;
 import java.util.Scanner;
 
 public class Main {
-    static int[] combination;
-    static int n, m;
+    static int[] COMBINATION;
+    static int N, M; // N - 숫자 범위(N개의 수), M - 뽑을 갯수
 
-    public void DFS(int L, int s) {
-        if (L == m) {
-            for (int i : combination) {
+    public void dfs(int L, int start) {
+        if (L == M) {
+            for (int i : COMBINATION) {
                 System.out.print(i + " ");
             }
             System.out.println();
         } else {
-            for (int i = s; i <= n; i++) {
-                combination[L] = i;
-                DFS(L + 1, i + 1);
+            for (int i = start; i <= N; i++) {
+                COMBINATION[L] = i;
+                dfs(L + 1, i + 1);
             }
         }
     }
@@ -23,9 +23,9 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        combination = new int[m];
-        T.DFS(0, 1);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        COMBINATION = new int[M];
+        T.dfs(0, 1);
     }
 }
