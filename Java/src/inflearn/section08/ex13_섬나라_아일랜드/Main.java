b9e63891f83c150b3f544722cb779f69773843a8
gusrb8925 <gusrb8925@naver.com>
2021-08-29 20:03:43 +0900

    21-08-29 DFS(Depth-First Search) 활용

--- Java/src/inflearn/section08/ex13_섬나라_아일랜드/Main.java
+++ Java/src/inflearn/section08/ex13_섬나라_아일랜드/Main.java
@@ -3,28 +3,28 @@ package inflearn.section08.ex13_섬나라_아일랜드;
 import java.util.Scanner;
 
 public class Main {
-    static int answer = 0, n;
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int ANSWER = 0, N;
+    static int[] DX = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] DY = {0, 1, 1, 1, 0, -1, -1, -1};
 
-    private void DFS(int x, int y, int[][] board) {
+    private void dfs(int x, int y, int[][] board) {
         for (int i = 0; i < 8; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n && board[nextX][nextY] == 1) {
+            int nextX = x + DX[i];
+            int nextY = y + DY[i];
+            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && board[nextX][nextY] == 1) {
                 board[nextX][nextY] = 0;
-                DFS(nextX, nextY, board);
+                dfs(nextX, nextY, board);
             }
         }
     }
 
     public void solution(int[][] board) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (board[i][j] == 1) {
-                    answer++;
+                    ANSWER++;
                     board[i][j] = 0;
-                    DFS(i, j, board);
+                    dfs(i, j, board);
                 }
             }
         }
@@ -33,14 +33,17 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        N = sc.nextInt();
+
+        // arr init
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+
         T.solution(arr);
-        System.out.println(answer);
+        System.out.println(ANSWER);
     }
 }
