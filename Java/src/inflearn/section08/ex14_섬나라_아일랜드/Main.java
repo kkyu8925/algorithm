8dbf7b171eef4fc56b1c9ce21fb78430a3d2e399
gusrb8925 <gusrb8925@naver.com>
2021-08-29 20:06:15 +0900

    21-08-29 BFS(Breadth-First Search) 활용

--- Java/src/inflearn/section08/ex14_섬나라_아일랜드/Main.java
+++ Java/src/inflearn/section08/ex14_섬나라_아일랜드/Main.java
@@ -14,33 +14,33 @@ class Point {
 }
 
 public class Main {
-    static int answer = 0, n;
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int ANSWER = 0, N;
+    static int[] DX = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] DY = {0, 1, 1, 1, 0, -1, -1, -1};
 
-    private void solution(int[][] board) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
-                    answer++;
-                    board[i][j] = 0;
-                    BFS(i, j, board);
-                }
-            }
-        }
-    }
-
-    private void BFS(int x, int y, int[][] board) {
+    private void bfs(int x, int y, int[][] board) {
         Queue<Point> que = new LinkedList<>();
         que.offer(new Point(x, y));
         while (!que.isEmpty()) {
             Point now = que.poll();
             for (int i = 0; i < 8; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n && board[nextX][nextY] == 1) {
+                int nextX = now.x + DX[i];
+                int nextY = now.y + DY[i];
+                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && board[nextX][nextY] == 1) {
                     board[nextX][nextY] = 0;
                     que.add(new Point(nextX, nextY));
+                }
+            }
+        }
+    }
+
+    private void solution(int[][] board) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1) {
+                    ANSWER++;
+                    board[i][j] = 0;
+                    bfs(i, j, board);
                 }
             }
         }
@@ -49,14 +49,17 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        N = sc.nextInt();
+
+        // arr init
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+
         T.solution(arr);
-        System.out.println(answer);
+        System.out.println(ANSWER);
     }
 }
