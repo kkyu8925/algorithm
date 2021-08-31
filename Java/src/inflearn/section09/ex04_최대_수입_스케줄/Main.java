7bea0f5cc158a358d82549f9b67ffa8342d00f5e
gusrb8925 <gusrb8925@naver.com>
2021-08-31 16:41:59 +0900

    21-08-31 Greedy algorithm & PriorityQueue & Comparable

--- Java/src/inflearn/section09/ex04_최대_수입_스케줄/Main.java
+++ Java/src/inflearn/section09/ex04_최대_수입_스케줄/Main.java
@@ -13,20 +13,22 @@ class Lecture implements Comparable<Lecture> {
 
     @Override
     public int compareTo(Lecture o) {
-        return o.date - this.date;
+        return o.date - this.date; // 내림차순
     }
 }
 
 public class Main {
-    static int n, max = 0;
+    static int N, MAX_DATE = 0;
 
     private int solution(List<Lecture> arr) {
         int answer = 0;
-        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
         Collections.sort(arr);
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 마지막 날부터 할 수 있는 날짜 확인
         int j = 0;
-        for (int i = max; i >= 1; i--) {
-            for (; j < n; j++) {
+        for (int i = MAX_DATE; i >= 1; i--) {
+            for (; j < N; j++) {
                 if (arr.get(j).date < i) {
                     break;
                 } else {
@@ -43,13 +45,13 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+        N = sc.nextInt();
         List<Lecture> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             int money = sc.nextInt();
             int date = sc.nextInt();
             arr.add(new Lecture(money, date));
-            max = Math.max(max, date);
+            MAX_DATE = Math.max(MAX_DATE, date);
         }
         System.out.println(T.solution(arr));
     }
