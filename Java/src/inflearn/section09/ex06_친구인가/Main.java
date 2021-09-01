dbd44bd8e8d16d0f8311a161a0e7025bad7571bc
gusrb8925 <gusrb8925@naver.com>
2021-09-01 15:27:10 +0900

    21-09-01 Disjoint-Set(서로소 집합) : Union&Find

--- Java/src/inflearn/section09/ex06_친구인가/Main.java
+++ Java/src/inflearn/section09/ex06_친구인가/Main.java
@@ -3,13 +3,16 @@ package inflearn.section09.ex06_친구인가;
 import java.util.Scanner;
 
 public class Main {
-    static int[] unf;
+    static int[] UNF;
 
     private static int find(int idx) {
-        if (idx == unf[idx]) {
+        // 인덱스와 인덱스의 배열값이 같을 때까지 순환
+        if (idx == UNF[idx]) {
             return idx;
         } else {
-            return unf[idx] = find(unf[idx]);
+            // 시간복잡도 해결 - 여러개의 자식 노드를 가진 트리로 압축
+//            return find(UNF[idx]);
+            return UNF[idx] = find(UNF[idx]);
         }
     }
 
@@ -17,27 +20,35 @@ public class Main {
         int fa = find(a);
         int fb = find(b);
         if (fa != fb) {
-            unf[fa] = fb;
+            UNF[fa] = fb;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        unf = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            unf[i] = i;
+        int n = sc.nextInt(); // 학생 수
+        int m = sc.nextInt(); // 관계 수
+
+        // 서로소 집합 init
+        UNF = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            UNF[i] = i;
         }
+
         for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             union(a, b);
         }
+
+        // 마지막 입력
         int a = sc.nextInt();
         int b = sc.nextInt();
+
+        // a,b 는 친구인가?
         int fa = find(a);
         int fb = find(b);
+
         if (fa == fb) {
             System.out.println("YES");
         } else {
