1fcfdd53b3e17176c803b7a1aa85b3ac6e3af5a1
gusrb8925 <gusrb8925@naver.com>
2021-09-01 16:32:52 +0900

    21-09-01 MST(Minimum Spanning Tree) : Prim's algorithm , PriorityQueue

--- Java/src/inflearn/section09/ex08_원더랜드/Main.java
+++ Java/src/inflearn/section09/ex08_원더랜드/Main.java
@@ -25,30 +25,35 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+
         List<ArrayList<Edge>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
+
         int[] check = new int[n + 1];
         for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            graph.get(a).add(new Edge(b, c));
-            graph.get(b).add(new Edge(a, c));
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            int weight = sc.nextInt();
+            graph.get(v1).add(new Edge(v2, weight));
+            graph.get(v2).add(new Edge(v1, weight));
         }
+
         int answer = 0;
         PriorityQueue<Edge> pQ = new PriorityQueue<>();
         pQ.offer(new Edge(1, 0));
         while (!pQ.isEmpty()) {
             Edge now = pQ.poll();
-            int nextVex = now.vex;
-            if (check[nextVex] == 0) {
-                check[nextVex] = 1;
+            int endVex = now.vex;
+
+            // 방문한 노드인지 확인
+            if (check[endVex] == 0) {
+                check[endVex] = 1;
                 answer += now.cost;
-                for (Edge edge : graph.get(nextVex)) {
-                    if (check[edge.vex] == 0) {
-                        pQ.offer(new Edge(edge.vex, edge.cost));
+                for (Edge next : graph.get(endVex)) {
+                    if (check[next.vex] == 0) {
+                        pQ.offer(new Edge(next.vex, next.cost));
                     }
                 }
             }
