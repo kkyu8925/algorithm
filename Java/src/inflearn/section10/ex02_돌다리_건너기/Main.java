139e10a1544bbaa13796d0bcaf6c2750ce6cd23f
gusrb8925 <gusrb8925@naver.com>
2021-09-02 15:20:41 +0900

    21-09-02 dynamic programming

--- Java/src/inflearn/section10/ex02_돌다리_건너기/Main.java
+++ Java/src/inflearn/section10/ex02_돌다리_건너기/Main.java
@@ -3,22 +3,23 @@ package inflearn.section10.ex02_돌다리_건너기;
 import java.util.Scanner;
 
 public class Main {
-    static int[] dy;
+    static int[] DY;
 
     private int solution(int n) {
-        dy[1] = 1;
-        dy[2] = 2;
-        for (int i = 3; i <= n + 1; i++) {
-            dy[i] = dy[i - 2] + dy[i - 1];
+        // N(3<=N<=35)
+        DY[1] = 1;
+        DY[2] = 2;
+        for (int i = 3; i < n + 2; i++) {
+            DY[i] = DY[i - 2] + DY[i - 1];
         }
-        return dy[n + 1];
+        return DY[n + 1];
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        dy = new int[n + 2];
+        DY = new int[n + 2];
         System.out.print(T.solution(n));
     }
 }
