ce5733de9348ad66a16bf4dc5f0293c012827d52
gusrb8925 <gusrb8925@naver.com>
2021-09-02 16:26:28 +0900

    21-09-02 dynamic programming : Knapsack Problem

--- Java/src/inflearn/section10/ex05_동전교환/Main.java
+++ Java/src/inflearn/section10/ex05_동전교환/Main.java
@@ -3,32 +3,35 @@ package inflearn.section10.ex05_동전교환;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// 동전의 종류개숙가 많으면 DFS 시간복잡도 문제발생
+// section08.ex05_동전교환
 public class Main {
-    static int n, m;
-    static int[] dy;
+    static int MONEY;
+    static int[] DY;
 
-    private int solution(int[] coin) {
-        Arrays.fill(dy, Integer.MAX_VALUE);
-        dy[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = coin[i]; j <= m; j++) {
-                dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+    private int solution(int[] coins) {
+        Arrays.fill(DY, Integer.MAX_VALUE);
+        DY[0] = 0;
+        for (int coin : coins) {
+            for (int j = coin; j <= MONEY; j++) {
                 // 예를 들어, 5원이 들어오면 5-5=0, dy[0]=0+1 > 5원 동전 하나만 걸러주면 된다.
+                // 목표금액까지 반복
+                DY[j] = Math.min(DY[j], DY[j - coin] + 1);
             }
         }
-        return dy[m];
+        return DY[MONEY];
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        m = sc.nextInt();
-        dy = new int[m + 1];
+        MONEY = sc.nextInt();
+        DY = new int[MONEY + 1];
         System.out.println(T.solution(arr));
     }
 }
