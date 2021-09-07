8e8ea7ace58f1fa42d283d7de211c117c9870f1e
gusrb8925 <gusrb8925@naver.com>
2021-09-07 20:49:07 +0900

    21-09-07 stack/que

--- Java/src/programmers/stackque/lv2_다리를_지나는_트럭/Solution.java
+++ Java/src/programmers/stackque/lv2_다리를_지나는_트럭/Solution.java
@@ -1,38 +1,61 @@
 package programmers.stackque.lv2_다리를_지나는_트럭;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
+class Truck {
+    int weight;
+    int move;
+
+    public Truck(int weight) {
+        this.weight = weight;
+        this.move = 1;
+    }
+
+    public void moving() {
+        move++;
+    }
+}
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+
+        for (int t : truck_weights) {
+            waitQ.offer(new Truck(t));
+        }
+
         int answer = 0;
-        Queue<Integer> que = new LinkedList<>();
-        int max = 0;
+        int currentWeight = 0;
 
-        for (int truck : truck_weights) {
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
 
-            while (true) {
-                if (que.isEmpty()) {
-                    que.offer(truck);
-                    max += truck;
-                    answer++;
-                    break;
-                } else {
-                    if (que.size() == bridge_length) {
-                        max -= que.poll();
-                    } else if (max + truck > weight) {
-                        que.offer(0);
-                        answer++;
-                    } else {
-                        que.offer(truck);
-                        max += truck;
-                        answer++;
-                        break;
-                    }
-                }
+            answer++;
+
+            if (moveQ.isEmpty()) {
+                Truck truck = waitQ.poll();
+                currentWeight += truck.weight;
+                moveQ.offer(truck);
+                continue;
+            }
+
+            for (Truck truck : moveQ) {
+                truck.moving();
+            }
+
+            if (moveQ.peek().move > bridge_length) {
+                Truck truck = moveQ.poll();
+                currentWeight -= truck.weight;
+            }
+
+            if (!waitQ.isEmpty() && currentWeight + waitQ.peek().weight <= weight) {
+                Truck truck = waitQ.poll();
+                currentWeight += truck.weight;
+                moveQ.offer(truck);
             }
         }
-        return answer + bridge_length;
-    }
 
+        return answer;
+    }
 }
---
+++ Java/src/programmers/stackque/lv2_다리를_지나는_트럭/Solution2.java
@@ -0,0 +1,38 @@
+package programmers.stackque.lv2_다리를_지나는_트럭;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution2 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> que = new LinkedList<>();
+        int max = 0;
+
+        for (int truck : truck_weights) {
+
+            while (true) {
+                if (que.isEmpty()) {
+                    que.offer(truck);
+                    max += truck;
+                    answer++;
+                    break;
+                } else {
+                    if (que.size() == bridge_length) {
+                        max -= que.poll();
+                    } else if (max + truck > weight) {
+                        que.offer(0);
+                        answer++;
+                    } else {
+                        que.offer(truck);
+                        max += truck;
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer + bridge_length;
+    }
+
+}
