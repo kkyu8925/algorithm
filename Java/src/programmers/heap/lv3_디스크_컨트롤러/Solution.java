3a5654f019b3124dc2daaee820d0f17e67a47b9e
gusrb8925 <gusrb8925@naver.com>
2021-09-09 16:18:02 +0900

    21-09-09 heap

---
+++ Java/src/programmers/heap/lv3_디스크_컨트롤러/Solution.java
@@ -0,0 +1,41 @@
+package programmers.heap.lv3_디스크_컨트롤러;
+
+import java.util.*;
+
+class Solution {
+
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        int cnt = 0;
+        int nowTime;
+
+        Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
+        nowTime = jobs[0][0];
+
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+
+        int i = 0;
+        while (cnt < jobs.length) {
+            while (i < jobs.length && jobs[i][0] <= nowTime) {
+                queue.add(jobs[i++]);
+            }
+
+            if (!queue.isEmpty()) {
+                int[] job = queue.poll();
+                nowTime += job[1];
+                answer += nowTime - job[0];
+                cnt++;
+            } else {
+                nowTime = jobs[i][0];
+            }
+        }
+
+        return answer / cnt;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        System.out.println(T.solution(jobs));
+    }
+}
